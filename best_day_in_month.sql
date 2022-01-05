becee6a3efda6c0095bce5b1845fefea3549f8a0
J-Concepcion <jconcepcion@gmail.com>
2022-01-04 19:04:30 -0800

    Update best_day_in_month.sql

--- best_day_in_month.sql
+++ best_day_in_month.sql
@@ -1,5 +1,6 @@
 -- Using SNOWFLAKE_SAMPLE_DATE.TPCH_SF1
 
+
 -- Method using a window function
 with 
 daily_sales as
@@ -31,3 +32,37 @@ select a.* from daily_sales a
 join maxb b on a.tot = b.maxtot and a.sales_month = b.sales_month
 order by 1,2
 ;
+
+-- Correlated subquery
+with 
+daily_sales as
+(
+select date_trunc(month, O_ORDERDATE) as sales_month, O_ORDERDATE, sum(o_totalprice) as tot from orders 
+group by 1,2
+)
+select a.* from daily_sales a
+where tot =
+(select max(tot) from daily_sales b where sales_month = a.sales_month
+)
+order by 1,2
+;
+
+-- This kind of subquery is unsupported
+with 
+daily_sales as
+(
+select date_trunc(month, O_ORDERDATE) as sales_month, O_ORDERDATE, sum(o_totalprice) as tot from orders 
+group by 1,2
+),
+maxb as
+(
+select sales_month, max(tot) as maxtot from daily_sales group by 1
+)
+--select * from maxb
+select a.* from daily_sales a
+where tot =
+(select maxtot from maxb b where maxtot = a.tot and sales_month = a.sales_month
+)
+;
+-- Result: Unsupported subquery type cannot be evaluated
+
